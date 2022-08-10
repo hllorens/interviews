6f95c86950e61fe8c86d888b58145df8f21fdc70
Hector Llorens <hectorlm1983@gmail.com>
2022-08-10 16:56:32 +0200

    stack

--- class_stack.py
+++ class_stack.py
@@ -18,6 +18,11 @@ class Stack:
   def __str__(self):
     return 'value='+str(self.value)+' pointer='+self.hasPointer()
 
+  def pop(self):
+    v=self.value
+    if self.pointer is not None:
+      self=self.p
+    return v
 
 s=Stack()
 print(s)
@@ -25,5 +30,7 @@ s.value='2'
 print(s)
 sParent=Stack('1')
 s.link(sParent)
+print(s)
+s.pop()
 print(s)
 
