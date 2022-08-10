4e0a00ff56abe33b1d1f54ea975b98c6095617b7
Hector Llorens <hectorlm1983@gmail.com>
2022-08-10 16:43:41 +0200

    stack

--- class_stack.py
+++ class_stack.py
@@ -1,10 +1,29 @@
-class Stack (self):
-   _init_(self):
-      bla bla
+class Stack:
+  value=None
+  pointer=None
+  def __init__(self, v=None, p=None):
+    self.value=v
+    self.pointer=p
+
+  def link(self,p):
+    if isinstance(p,Stack):
+      self.pointer=p
+    else:
+      print('Error linking')
+  def hasPointer(self):
+    if self.pointer is None:
+      return 'False'
+    return 'True'
+
+  def __str__(self):
+    return 'value='+str(self.value)+' pointer='+self.hasPointer()
 
 
-s=new Stack()
-
-
-
+s=Stack()
+print(s)
+s.value='2'
+print(s)
+sParent=Stack('1')
+s.link(sParent)
+print(s)
 
